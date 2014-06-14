ede0df6a96fdf7e367f868b530121b8ac7bbf5f1
Johan Mynhardt <johan.mynhardt@thumbtribe.mobi>
2014-06-14 15:57:38 +0200

    File upload working

--- src/main/java/za/co/johanmynhardt/pvps/service/rest/FileService.java
+++ src/main/java/za/co/johanmynhardt/pvps/service/rest/FileService.java
@@ -1,7 +1,14 @@
 package za.co.johanmynhardt.pvps.service.rest;
 
+import java.io.InputStream;
+
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.core.MediaType;
+
+import com.sun.jersey.multipart.FormDataParam;
 
 @Path("/file")
 public class FileService {
@@ -9,6 +16,14 @@ public class FileService {
 	@GET
 	public String getDefault() {
 		return "pong!";
+	}
+
+	@POST
+	@Consumes(MediaType.MULTIPART_FORM_DATA)
+	@Path("/upload")
+	public String consumeUpload(@FormDataParam("file")InputStream body) {
+			System.out.println("inputStream == null: " + body == null);
+			return "success";
 	}
 }
 // vim: tabstop=2
