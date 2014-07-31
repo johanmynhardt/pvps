47b5b48b8ff8085b0389c531e44215b117089cec
Johan Mynhardt <johanmynhardt@gmail.com>
2014-07-31 07:04:26 +0200

    switching to resteasy

--- src/main/java/za/co/johanmynhardt/pvps/service/rest/FileService.java
+++ src/main/java/za/co/johanmynhardt/pvps/service/rest/FileService.java
@@ -10,9 +10,11 @@ import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.core.MediaType;
 
-import com.sun.jersey.multipart.FormDataParam;
+import org.jboss.resteasy.annotations.providers.multipart.MultipartForm;
 import za.co.johanmynhardt.pvps.service.ImageService;
 import za.co.johanmynhardt.pvps.service.impl.ImageServiceImpl;
+
+import org.jboss.resteasy.plugins.providers.multipart.InputPart;
 
 @Path("/file")
 public class FileService {
@@ -27,7 +29,7 @@ public class FileService {
 	@POST
 	@Consumes(MediaType.MULTIPART_FORM_DATA)
 	@Path("/upload")
-	public String consumeUpload(@FormDataParam("file") InputStream body) {
+	public String consumeUpload(@MultipartForm("file") InputStream body) {
 
 		try {
 			File file = imageService.resizeImage(body);
