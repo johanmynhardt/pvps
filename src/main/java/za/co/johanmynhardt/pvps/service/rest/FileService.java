83209b6d6cf1158d7d7335be9a9b4bfe92973984
Johan Mynhardt <johan.mynhardt@thumbtribe.mobi>
2014-08-02 06:51:42 +0200

    RESTeasy implementation in use.
    Much more verbose..

--- src/main/java/za/co/johanmynhardt/pvps/service/rest/FileService.java
+++ src/main/java/za/co/johanmynhardt/pvps/service/rest/FileService.java
@@ -3,6 +3,12 @@ package za.co.johanmynhardt.pvps.service.rest;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.logging.Logger;
 
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
@@ -10,14 +16,14 @@ import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.core.MediaType;
 
-import org.jboss.resteasy.annotations.providers.multipart.MultipartForm;
+import org.jboss.resteasy.plugins.providers.multipart.InputPart;
+import org.jboss.resteasy.plugins.providers.multipart.MultipartFormDataInput;
 import za.co.johanmynhardt.pvps.service.ImageService;
 import za.co.johanmynhardt.pvps.service.impl.ImageServiceImpl;
 
-import org.jboss.resteasy.plugins.providers.multipart.InputPart;
-
 @Path("/file")
 public class FileService {
+	private static Logger logger = Logger.getLogger(FileService.class.getName());
 
 	ImageService imageService = new ImageServiceImpl();
 
@@ -29,17 +35,61 @@ public class FileService {
 	@POST
 	@Consumes(MediaType.MULTIPART_FORM_DATA)
 	@Path("/upload")
-	public String consumeUpload(@MultipartForm("file") InputStream body) {
+	public String consumeUpload(MultipartFormDataInput input) {
+		Map<String, InputStream> attachmentMap = FormUtil.getInputStreams(input);
+
+		System.out.println("attachmentMap = " + attachmentMap.keySet());
 
 		try {
-			File file = imageService.resizeImage(body);
-			System.out.println("file = " + file);
+
+			for (InputStream inputStream : attachmentMap.values()) {
+				File file = imageService.resizeImage(inputStream);
+				System.out.println("file = " + file);
+			}
 		} catch (IOException e) {
 			e.printStackTrace();
 			throw new RuntimeException(e);
 		}
 
 		return "success";
+	}
+
+	private static class FormUtil {
+		public static Map<String, InputStream> getInputStreams(MultipartFormDataInput form) {
+			Map<String, InputStream> inputMap = Collections.emptyMap();
+
+			Map<String, List<InputPart>> formData = form.getFormDataMap();
+
+			for (String key : formData.keySet()) {
+				for (InputPart inputPart : formData.get(key)) {
+					if (inputPart.getHeaders().containsKey("Content-Disposition")) {
+						List<String> values = Arrays.asList(inputPart.getHeaders().get("Content-Disposition").get(0).split("; "));
+						String filename = null;
+						for (String value : values) {
+							if (value.startsWith("filename=")) {
+								filename = value.split("filename=",2)[1];
+								if (filename.contains("\"")) {
+									filename = filename.replaceAll("\"", "");
+								}
+								break;
+							}
+						}
+
+						if (filename != null) {
+							if (inputMap.isEmpty()) {
+								inputMap = new HashMap<String, InputStream>();
+							}
+							try {
+								inputMap.put(filename, inputPart.getBody(InputStream.class, null));
+							} catch (IOException e) {
+								e.printStackTrace();
+							}
+						}
+					}
+				}
+			}
+			return inputMap;
+		}
 	}
 }
 // vim: tabstop=2
